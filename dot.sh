a3bdbaddc723e695b86a3345420da8c51053f235
Vlad <vladstudio@rambler.ru>
2015-08-07 01:55:17 +0300

    fixed wrong symlinks && removed nimcache

--- dot.sh
+++ dot.sh
@@ -3,7 +3,7 @@ mkdir -p ~/.config
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.conkyrc ~/.conkyrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/htop ~/.config/htop
+ln -s ~/dotfiles/htop ~/.config/
 ln -s ~/dotfiles/termite ~/.config/
 ln -s ~/dotfiles/sublime-text-3 ~/.config/
 ln -s ~/dotfiles/.dircolors ~/
