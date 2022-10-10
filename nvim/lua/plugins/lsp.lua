2e368a7e86e25850779b788d0a9a0f122c14b6da
HaCk3D <vladstudiorus@gmail.com>
2022-10-10 14:14:15 +0200

    lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,13 +14,22 @@ local servers_with_default_settings = {
   'jsonls',
   'dockerls',
 }
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = { "documentation", "detail", "additionalTextEdits" },
+}
+
 for _, server in ipairs(servers_with_default_settings) do
   lspconfig[server].setup({
     capabilities = capabilities,
-    on_attach = function(_, bufnr)
-      require "lsp_signature".on_attach(signature_setup, bufnr)
-    end,
+
+    require('lsp_signature').on_attach({
+      bind = true,
+      hint_prefix = '💡',
+    }, bufnr)
+
   })
 end
 
