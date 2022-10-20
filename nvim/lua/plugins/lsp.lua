6958f583285e5106c125542a573e17d01c9125da
HaCk3D <vladstudiorus@gmail.com>
2022-10-20 08:45:54 +0200

    update def cap

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -36,7 +36,7 @@ local servers_with_default_settings = {
   "dockerls",
 }
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(basic_capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
   properties = { "documentation", "detail", "additionalTextEdits" },
