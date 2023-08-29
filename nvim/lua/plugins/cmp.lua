bfa2b8aa1e5d6b0f55ed68b968e404dca475f07c
HaCk3D <vladstudiorus@gmail.com>
2023-08-29 16:05:17 +0200

    fixed lua deprecated unpack + new swap hotkey

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,7 +4,7 @@ local luasnip = require("luasnip")
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
@@ -14,7 +14,6 @@ cmp.setup({
       luasnip.lsp_expand(args.body)
     end,
   },
-
   mapping = {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
@@ -31,8 +30,8 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+      elseif luasnip.jumpable( -1) then
+        luasnip.jump( -1)
       else
         fallback()
       end
@@ -41,17 +40,14 @@ cmp.setup({
     ["<C-E>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm(),
   },
-
   sources = cmp.config.sources({
     { name = "nvim_lua", max_item_count = 10 },
     { name = "nvim_lsp", max_item_count = 10 },
     { name = "luasnip" },
-    { name = "path", max_item_count = 10 },
-    { name = "buffer", keyword_length = 4, max_item_count = 10 },
+    { name = "path",     max_item_count = 10 },
+    { name = "buffer",   keyword_length = 4, max_item_count = 10 },
   }),
-
   preselect = cmp.PreselectMode.None,
-
   formatting = {
     format = lspkind.cmp_format({
       mode = "text",
---
+++ swap-audio.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import pulsectl
+
+headphones = 'alsa_output.usb-Macronix_Razer_Barracuda_X_2.4_1234-00.analog-stereo'
+speakers = 'alsa_output.pci-0000_00_1f.3.analog-stereo'
+
+with pulsectl.Pulse('sink-swapper') as pulse:
+
+    sink_list = pulse.sink_list()
+    sink_names = [e.name for e in pulse.sink_list()]
+    if headphones in sink_names:
+
+        current = pulse.server_info().default_sink_name
+        swapped = speakers if current == headphones else headphones
+
+        found_sink = list(filter(lambda e: e.name == swapped, sink_list))
+        if found_sink:
+            pulse.default_set(found_sink[0])
