e4f6a585273b710b9a1bb20785c1cd51ae30204b
HaCk3D <vladstudiorus@gmail.com>
2023-08-29 19:07:58 +0200

    refactor

--- swap-audio.py
+++ swap-audio.py
@@ -5,14 +5,10 @@ headphones = 'alsa_output.usb-Macronix_Razer_Barracuda_X_2.4_1234-00.analog-ster
 speakers = 'alsa_output.pci-0000_00_1f.3.analog-stereo'
 
 with pulsectl.Pulse('sink-swapper') as pulse:
-
     sink_list = pulse.sink_list()
-    sink_names = [e.name for e in sink_list]
-    if headphones in sink_names:
-
-        current = pulse.server_info().default_sink_name
-        swapped = speakers if current == headphones else headphones
-
+    if headphones in [e.name for e in sink_list]:
+        swapped = speakers if pulse.server_info()\
+            .default_sink_name == headphones else headphones
         found_sink = list(filter(lambda e: e.name == swapped, sink_list))
         if found_sink:
             pulse.default_set(found_sink[0])
