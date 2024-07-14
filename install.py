c0601ea14c388c81207187531912ed6988d12bb9
HaCk3D <vladstudiorus@gmail.com>
2024-07-14 12:36:35 +0200

    gpg agent

--- install.py
+++ install.py
@@ -2,41 +2,42 @@ from os import system
 from sys import argv
 
 config_destinations = {
-    '.gitconfig': '~/',
-    '.xinitrc': '~/',
-    '.zshrc': '~/',
-    '.gtkrc-2.0': '~/',
-    '.tmux.conf': '~/',
-    '.p10k.zsh': '~/',
-    'dunst': '~/.config/',
-    'gtk-3.0': '~/.config/',
-    'gtk-4.0': '~/.config/',
-    'htop': '~/.config/',
-    'i3': '~/.config/',
-    'nvim': '~/.config/',
-    'picom': '~/.config/',
-    'polybar': '~/.config/',
-    'alacritty': '~/.config/',
-    'zathura': '~/.config/',
-    'feh': '~/.config/',
-    'rofi': '~/.config/',
-    'qt5ct': '~/.config/',
-    'lazygit': '~/.config/',
-    'layout.service': '~/.config/systemd/user/',
+    ".gitconfig": "~/",
+    ".xinitrc": "~/",
+    ".zshrc": "~/",
+    ".gtkrc-2.0": "~/",
+    ".tmux.conf": "~/",
+    ".p10k.zsh": "~/",
+    "dunst": "~/.config/",
+    "gpg-agent.conf": "~/.gnupg/",
+    "gtk-3.0": "~/.config/",
+    "gtk-4.0": "~/.config/",
+    "htop": "~/.config/",
+    "i3": "~/.config/",
+    "nvim": "~/.config/",
+    "picom": "~/.config/",
+    "polybar": "~/.config/",
+    "alacritty": "~/.config/",
+    "zathura": "~/.config/",
+    "feh": "~/.config/",
+    "rofi": "~/.config/",
+    "qt5ct": "~/.config/",
+    "lazygit": "~/.config/",
+    "layout.service": "~/.config/systemd/user/",
 }
 
 server_part = [
-    '.zshrc',
-    '.tmux.conf',
-    '.p10k.zsh',
-    'nvim',
-    'htop',
+    ".zshrc",
+    ".tmux.conf",
+    ".p10k.zsh",
+    "nvim",
+    "htop",
 ]
 
 for config, path in config_destinations.items():
-    if len(argv) > 1 and argv[1] == '--server' and config not in server_part:
+    if len(argv) > 1 and argv[1] == "--server" and config not in server_part:
         continue
 
-    system(f'rm -r {path}{config}')
-    system(f'ln -s $PWD/{config} {path}{config}')
-    print(f'installed: {path}{config}')
+    system(f"rm -r {path}{config}")
+    system(f"ln -s $PWD/{config} {path}{config}")
+    print(f"installed: {path}{config}")
