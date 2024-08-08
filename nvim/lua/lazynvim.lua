2d234d94b440874205cdd392a46eeae37070ae15
HaCk3D <vladstudiorus@gmail.com>
2024-08-08 19:14:19 +0200

    add editorconfig

--- nvim/lua/lazynvim.lua
+++ nvim/lua/lazynvim.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-	},
-	checker = { enabled = true, notify = false },
-	change_detection = { notify = false },
+  spec = {
+    { import = "plugins" },
+  },
+  checker = { enabled = true, notify = false },
+  change_detection = { notify = false },
 })
 
 require("lazy.view.config").keys.close = "<esc>"
