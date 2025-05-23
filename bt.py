281bc8b11bcf8306ecb898a5beee53fcff5f15f2
HaCk3D <vladstudiorus@gmail.com>
2025-05-23 22:33:42 +0200

    feat: polybar kb battery

---
+++ bt.py
@@ -0,0 +1,34 @@
+import dbus
+
+BT_ICON = ""
+LOW_BAT = 15
+RED_COLOR = "%{F#FF4444}"
+RESET_COLOR = "%{F-}"
+
+
+def main() -> None:
+    bus = dbus.SystemBus()
+    manager = dbus.Interface(
+        bus.get_object("org.bluez", "/"), "org.freedesktop.DBus.ObjectManager"
+    )
+
+    for _, interface in manager.GetManagedObjects().items():
+        if "org.bluez.Battery1" not in interface:
+            continue
+
+        perc = int(interface["org.bluez.Battery1"].get("Percentage", -1))
+
+        if perc >= 0:
+            perc_text = f"{perc}%"
+
+            if perc <= LOW_BAT:
+                perc_text = f"{RED_COLOR}{perc_text}{RESET_COLOR}"
+
+            print(f"{BT_ICON} {perc_text}")
+            exit()
+
+    print("")
+
+
+if __name__ == "__main__":
+    main()
