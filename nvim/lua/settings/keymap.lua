7fd1f64d884d2a9c5ff6ecedb78c4516c4a1a96a
HaCk3D <vladstudiorus@gmail.com>
2025-08-21 01:13:11 +0200

    fix: switch to telescope aerial

--- nvim/lua/settings/keymap.lua
+++ nvim/lua/settings/keymap.lua
@@ -42,7 +42,7 @@ map("n", "<space>r", ":Telescope lsp_references<CR>", opts)
 map("n", "<space>R", ":lua vim.lsp.buf.rename()<CR>", opts)
 map("n", "<space>s", ":Telescope lsp_document_symbols<CR>", opts)
 map("n", "<space>e", ":lua vim.diagnostic.open_float()<CR>", opts)
-map("n", "<space>a", ":AerialToggle<CR>", opts)
+map("n", "<space>a", ":Telescope aerial<CR>", opts)
 
 -- Folds
 map("n", "za", ":lua require('ufo').openAllFolds()<CR>", opts)
