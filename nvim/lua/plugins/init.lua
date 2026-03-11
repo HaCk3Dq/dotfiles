4c413c1f1bfa79ef449dcca82871e0b71df8dc35
HaCk3D <vladstudiorus@gmail.com>
2026-03-11 22:32:25 +0100

    feat: add spider

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,6 +13,16 @@ return {
   { "xiyaowong/transparent.nvim", priority = 1000 },
 
   {
+    "chrisgrieser/nvim-spider",
+    keys = {
+      { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+      { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+      { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+      { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+    },
+  },
+
+  {
     "levouh/tint.nvim",
     config = function()
       require("tint").setup({})
