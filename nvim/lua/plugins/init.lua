ea9f3b93c5d6bf05108a13f420ab9b70e998ca7b
HaCk3D <vladstudiorus@gmail.com>
2026-03-17 22:57:40 +0100

    chore: add typing

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,6 +4,8 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
     opts = {
       indent = { char = "▏" },
       scope = { enabled = false },
